f1775456207416c2d9e4d3979c02f3a0a100636f
yodatime <electriclikethor@gmail.com>
2025-02-13 18:44:53 +0200

    Ready

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace ConsoleApp1
+﻿using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            int x = int.Parse(Console.ReadLine());
+            if (IsPower(x))
+            {
+                Console.WriteLine("yes");
+            }else
+            {
+                Console.WriteLine("no");
+            }
+        }
+        
+        static bool IsPower(int x)
+        {
+            
+            if(x == 1) return true;
+            if(x % 2 != 0) return false;
+
+            return IsPower(x/2);
         }
     }
 }
